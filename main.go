0e9c0aa883fb2baaaf9b121093a1e84fc67290fa
jck <jck@jck-hydra.local>
2022-01-06 01:15:36 +0800

    test2

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"gin_mall/config"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+	"github.com/spf13/viper"
 )
 
 // 声明一个全局的rdb变量
@@ -23,6 +25,17 @@ func initClient() (err error) {
 	}
 	return nil
 }
+
+func InitConfig() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+}
+
 func main() {
 	r := gin.Default()
 	config.SetRouters(r)
