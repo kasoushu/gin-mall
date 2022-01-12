29f00a47572605099cdf0723ea41597f61805743
jck <jck@jck-hydra.local>
2022-01-12 23:38:23 +0800

    1/12/23:33

--- service/category.go
+++ service/category.go
@@ -1,1 +1,17 @@
 package service
+
+import "gin_mall/model"
+
+func InsertCategory(c model.Category) bool {
+	return true
+}
+func DeleteCategory(id uint64) bool {
+	return true
+}
+
+func UpdateCategory(id uint64, c model.Category) bool {
+	return true
+}
+func GetCategoryInfo(id uint64, c *model.Category) bool {
+	return true
+}
